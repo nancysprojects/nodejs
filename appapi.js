26145a446d496ef4da02015aa9f916c550c4d12b
Nancy <nancysprojects@users.noreply.github.com>
2019-10-05 18:01:57 -0700

    Update appapi.js

--- appapi.js
+++ appapi.js
@@ -1,13 +1,16 @@
-const http= require('http');
-const server = http.createServer((req,res) => {
-  if (req.url === '/'){
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
     res.write('Hello World');
     res.end();
   }
-  if (req.url === 'api/courses'){
-    res.write(JSON.stringify([a,b,c,d,e,f,g]));
-    res.end;
+
+  if (req.url === '/api/courses'){
+    res.write(JSON.stringify([1,2,3]))
+    res.end();
   }
 });
+
 server.listen(3000);
-console.log(Listening on port 3000);
+
+console.log('Listening on port 3000');
