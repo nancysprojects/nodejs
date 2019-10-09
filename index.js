4dd824db48fa7236a248d8424dbf21fd5eb11161
Nancy <nancysprojects@users.noreply.github.com>
2019-10-08 20:15:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/api/course')
+app.get('/api/courses', (req, res) => {
+     res.send([1, 2, 3]);
+
+});
 app.listen(3000, () => console.log('Listening on port 3000...'));
